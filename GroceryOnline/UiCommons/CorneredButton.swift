b1e2d556fa5e5eab7426a8b0815588a202c5221c
Peyman <peyman.osatian@gmail.com>
2024-11-03 21:29:14 +0100

    Day1

---
+++ GroceryOnline/UiCommons/CorneredButton.swift
@@ -0,0 +1,18 @@
+//
+//  CorneredButton.swift
+//  GroceryOnline
+//
+//  Created by Peyman Osatian on 2024-11-03.
+//
+
+import SwiftUI
+
+struct CorneredButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CorneredButton()
+}
