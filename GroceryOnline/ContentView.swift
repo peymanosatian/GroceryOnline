ce52d3844aed09eb7733d8c45488f0e27e1750a4
Peyman <peyman.osatian@gmail.com>
2024-11-03 16:27:38 +0100

    Initial Commit

---
+++ GroceryOnline/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  GroceryOnline
+//
+//  Created by Peyman Osatian on 2024-11-03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+          
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GroceryOnline/GroceryOnlineApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroceryOnlineApp.swift
+//  GroceryOnline
+//
+//  Created by Peyman Osatian on 2024-11-03.
+//
+
+import SwiftUI
+
+@main
+struct GroceryOnlineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
