3a81229b878d8fa418daea48a8b414ff39998c59
Peyman <peyman.osatian@gmail.com>
2024-11-10 23:12:56 +0100

    update210Nov

---
+++ GroceryOnline/Views/Login/SignInView.swift
@@ -0,0 +1,18 @@
+//
+//  SignInView.swift
+//  GroceryOnline
+//
+//  Created by Peyman Osatian on 2024-11-10.
+//
+
+import SwiftUI
+
+struct SignInView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SignInView()
+}
